65792659052dfce5d7f4a78498cd6084b79a7c1e
Leviethal <2024ucs0085@iitjammu.ac.in>
2024-12-31 20:28:38 +0530

    updated script

--- script.js
+++ script.js
@@ -2253,3 +2253,20 @@ function onMouseMove(event) {
 
 // Call the init function to set up the scene
 init();
+const left = document.querySelector(".left");
+const right = document.querySelector(".right");
+window.addEventListener("scroll", appearOnScroll)
+
+function appearOnScroll() {
+    let textPosition = left.getBoundingClientRect().top;
+    console.log(textPosition);
+    let windowHeight = window.innerHeight / 1.5;
+    if (windowHeight > textPosition) {
+        left.classList.remove("push-left");
+        right.classList.remove("push-right");
+    }
+    else {
+        left.classList.add("push-left");
+        right.classList.add("push-right");
+    }
+}
